651521b03bff5550a4881e305e2cc0a8512ea8e3
Cody Dupuis <codydupuis99@gmail.com>
2020-08-05 04:13:05 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
-# Add  code here!
+def prime?(int)
+   numbers = (2...int).to_a
+  if int < 0 || int == 0 || int == 1
+    return false
+  end
+  numbers.each do |num|
+    if int % num == 0
+      return false
+    end
+  end
+  return true
+end 
